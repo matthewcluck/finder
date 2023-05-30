ec59aa40ddb28ffc75fc77b505d395113a9df328
matthewcluck <matthewacluck@gmail.com>
2023-05-30 16:16:44 -0500

    CSS for add entry popup

--- src/Popup.js
+++ src/Popup.js
@@ -10,21 +10,21 @@ export default function Popup(props) {
         <>
           <div id="popup-overlay" onClick={props.overlayClose}></div>
           <div id="popup">
-          <div id="popup-close" onClick={props.close}>x</div>
+          <p id="popup-close" onClick={props.close}>x</p>
+          <br></br>
             <h2 id="popup-add">Add Trip Entry</h2>
             <form id="form">
-            <label id="form-location">Location:
-                <input name="location" />
-            </label>
-            <label id="form-rating">Rating:
-                <input name="rating" />
-            </label>
-            <label id="form-map">Map:
-                <input name="rating" />
-            </label>
-            <label id="form-description">Description:
-                <textarea name="description" />
-            </label>
+            <label id="form-location">Location: 
+            </label><input id="form-location-input" name="location" />
+
+            <label id="form-rating">Rating:  
+            </label><input id="form-rating-input" name="rating" />
+
+            <label id="form-map">Map: 
+            </label><input id="form-map-input" name="map" />
+
+            <label id="form-description">Description: 
+            </label><textarea id="form-description-input" name="description" />
             
             <button id="popup-submit"onClick={props.submission}>Submit</button>
             </form>
